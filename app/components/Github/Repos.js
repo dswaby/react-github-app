6c6ebec898293a414d0af33e121ec30109cd4188
Danny Swaby <danny@swa.by>
2016-02-23 14:00:59 -0600

    updating ui with github data

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -6,14 +6,18 @@ var Repos = React.createClass({
 		repos: React.PropTypes.array.isRequired
 	},
 	render: function() {
-		// repos = this.state.repos.map(function(repo){
-		// 	return <li className="list-group-item">{repo}</li>
-		// })
+		var repos = this.props.repos.map(function(repo, i){
+			return <li className="list-group-item" key={i}>
+				{repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+				{repo.description && <p>{repo.description}</p>}
+			</li>
+		})
 		
 		return (
 			<div className="Repos">
 				<h3>Repos</h3>
 				<ul className="list-group">
+					{repos}
 				</ul>
 			</div>
 		);
